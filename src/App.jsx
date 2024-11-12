ecf21bb2e587d7562fd67c42762f3f4ba72b3185
dracudev <andreujavier99@gmail.com>
2024-11-12 17:14:43 +0100

    LV1 - EX2 (nextStep)

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import { Card } from './components/Card'
 
 export default function App() {
   const [step, setStep] = useState(0);
+
   const tutorialData = [
     {
       title: "Dedica moltes hores",
       description: "Un mínim de 30 hores a la setmana. No en tens prou, harás de dedicar-li més hores. Al principi sembla impossible, però notarás una millora ràpidament.",
       bgColor: "",
       image: ""
+    },
+    {
+      title: "Programa projectes propis",
+      description: "Més val 10 hores treballan en projectes propis, que 10 hores mirant tutorials. La motivació y la implicació en el porjecte ajudará a accelerar el teu aprenentatge.",
+      bgColor: "",
+      image: ""
+    },
+    {
+      title: "Procura descansar",
+      description: "Descansar bé i desconectar són vitals. D'aquesta manera reduirás l'estrès i l'ansietat. Millorarás la teva concentració i consolidarás el teu aprenentatge.",
+      bgColor: "",
+      image: ""
     }
   ]
   
+  function nextStep() {
+    setStep((prevStep) => (prevStep + 1) % tutorialData.length);
+  }
 
 
   return (
@@ -21,6 +37,7 @@ export default function App() {
       description={tutorialData[step].description} 
       bgColor={tutorialData[step].bgColor} 
       image={tutorialData[step].image} 
+      nextStep={nextStep}
     />
   )
   
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
-export function Card({ title, description, bgColor, image }) {
+export function Card({ title, description, bgColor, image, nextStep }) {
     return (
         <>
             <h3>{title}</h3>
             <p>{description}</p>
+            <button onClick={nextStep}>Next Step</button>
         </>
     )
 }
