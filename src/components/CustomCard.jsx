ff93fe55e084bb4d845d9efb6ca28539f1ac42a2
dracudev <andreujavier99@gmail.com>
2024-11-13 12:37:13 +0100

    LV3 - EX7 (animations)

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import arrowRight from "../assets/arrowRight.webp";
 import arrowLeft from "../assets/arrowLeft.svg";
 import { Indicator } from "./Indicator";
+import { useState } from "react";
 
 export function CustomCard({
   title,
@@ -15,8 +16,27 @@ export function CustomCard({
   setStep,
   dataLength,
 }) {
+
+  const [animationClass, setAnimationClass] = useState('');
+
+  const handleNextStep = () => {
+    setAnimationClass('card-out-next');
+    setTimeout(() => {
+      nextStep();
+      setAnimationClass('card-in-next');
+    }, 700);
+  };
+
+  const handlePrevStep = () => {
+    setAnimationClass('card-out-prev');
+    setTimeout(() => {
+      prevStep();
+      setAnimationClass('card-in-prev');
+    }, 700);
+  };
+
   return (
-    <Card className="card">
+    <Card className={`card ${animationClass}`}>
       <Card.Img
         className="cardImage"
         variant="top"
@@ -32,17 +52,18 @@ export function CustomCard({
           step={step} 
           dataLength={dataLength} 
           setStep={setStep}
+          setAnimationClass={setAnimationClass}
           />
           <div className="cardButton">
             <button
               className={`${step === 0 ? "dNone" : ""}`}
-              onClick={prevStep}
+              onClick={handlePrevStep}
             >
               <img src={arrowLeft} alt="Left arrow" />
             </button>
             <button
               className={`${step == dataLength - 1 ? "dNone" : ""}`}
-              onClick={nextStep}
+              onClick={handleNextStep}
             >
               <img src={arrowRight} alt="Right arrow" />
             </button>
--- src/components/Indicator.jsx
+++ src/components/Indicator.jsx
@@ -12,25 +12,43 @@ const Dot = styled.div`
   border-radius: 50%;
   margin: 0 5px;
   transition: width 0.3s, height 0.3s;
+  cursor: pointer;
 
   &.active {
     width: 20px;
     height: 10px;
     background-color: black;
     border-radius: 5px;
+    cursor: default;
   }
 `;
 
-export function Indicator({ step, dataLength, setStep }) {
+export function Indicator({ step, dataLength, setStep, setAnimationClass }) {
 
-  function goToDot(index) {
-    setStep(index)
+  function goToDot(index, step) {
+    if (step === index)
+      return;
+    if (step < index){
+      setAnimationClass('card-out-next');
+      setTimeout(() => {
+      setStep(index);
+      setAnimationClass('card-in-next');
+    }, 700);
+    }
+    else {
+      setAnimationClass('card-out-prev');
+      setTimeout(() => {
+      setStep(index);
+      setAnimationClass('card-in-prev');
+    }, 700);
+    }
+    
   }
 
   return (
     <DotsContainer>
       {Array.from({ length: dataLength }).map((_, index) => (
-        <Dot key={index} className={index === step ? "active" : ""} onClick={() => goToDot(index)} />
+        <Dot key={index} className={index === step ? "active" : ""} onClick={() => goToDot(index, step)} />
       ))}
     </DotsContainer>
   );
